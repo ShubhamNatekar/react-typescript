a8099c97121a3c57fca7c23255ec458b8dbbbcc3
Shubham Natekar <shubham.natekar@external.atlascopco.com>
2026-03-07 10:57:49 +0530

    Added assessement-4 for stock frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./componants/index.css";
-import App from "./assessment-3/App";
+import App from "./assessment-4/App";
 
 // createRoot(document.getElementById('root')!).render(<BrowserRouter><Launcher /></BrowserRouter>)
 createRoot(document.getElementById("root")!).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
