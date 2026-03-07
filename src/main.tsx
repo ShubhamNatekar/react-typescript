dd81849892b3d82701435aad986ae12724c2934d
Shubham Natekar <shubham.natekar@external.atlascopco.com>
2026-03-07 12:38:28 +0530

    Refactored code changes:

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "./componants/index.css";
-import App from "./assessment-4/App";
+import Bubbles from "./assessment-5/App";
 
 // createRoot(document.getElementById('root')!).render(<BrowserRouter><Launcher /></BrowserRouter>)
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Bubbles />
 );
